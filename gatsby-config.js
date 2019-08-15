bcbeb831333e5de720dc1e1c873487b153c8fce3
Derrick <kang.derrick@gmail.com>
2019-08-15 12:00:05 +0900

    Update gatsby-config.js
    Changed the name

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@ module.exports = {
   siteMetadata: {
     url,
     siteUrl: url,
-    title: 'Blog by John Doe',
+    title: 'Blog by Derrick kang',
     subtitle:
       'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     copyright: '© All rights reserved.',
@@ -27,7 +27,7 @@ module.exports = {
       },
     ],
     author: {
-      name: 'John Doe',
+      name: 'Derrick kang',
       email: '#',
       telegram: '#',
       twitter: '#',
